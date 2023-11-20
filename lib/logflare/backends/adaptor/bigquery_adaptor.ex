bef9a4115f9d5b0bab6ba1a7b9efa13feeda2714
Łukasz Niemier <lukasz@niemier.pl>
2023-11-20 14:35:10 +0100

    ft: initial commit for BigQuery adaptor

---
+++ lib/logflare/backends/adaptor/bigquery_adaptor.ex
@@ -0,0 +1,24 @@
+defmodule Logflare.Backends.Adaptor.BigQueryAdaptor do
+  @moduledoc false
+
+  @behaviour Logflare.Backends.Adaptor
+
+  @impl Logflare.Backends.Adaptor
+  def ingest(_id, _events) do
+    raise "Unimplemented"
+  end
+
+  @impl Logflare.Backends.Adaptor
+  def execute_query(_id, _query),
+    do: {:error, :not_implemented}
+
+  @impl Logflare.Backends.Adaptor
+  def cast_config(params) do
+    {%{}, %{}}
+    |> Ecto.Changeset.cast(params, [])
+  end
+
+  @impl Logflare.Backends.Adaptor
+  def validate_config(changeset),
+    do: changeset
+end
